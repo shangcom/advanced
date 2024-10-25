51361f12dbffa81b9979ed0c053ef8c1495f9a35
shangcom <dndbs91@gmail.com>
2024-10-25 15:43:29 +0900

    스프링 핵심 원리 - 고급편
    섹션4 - 템플릿 메서드 패턴과 콜백 패턴
    
    템플릿 메서드 패턴 - 예제3
    
    완료

--- src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
+++ src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -39,9 +39,30 @@ public class TemplateMethodTest {
     void templateMethodV1() {
         AbstractTemplate template1 = new SubClassLogic1();
         template1.execute();
-
         AbstractTemplate template2 = new SubClassLogic2();
         template2.execute();
+    }
 
+    @Test
+    void templateMethodV2() {
+
+        AbstractTemplate template1 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+        log.info("template1의 익명 내부클래스의 이름 = {}", template1.getClass());
+
+        AbstractTemplate template2 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        };
+        log.info("template2의 익명 내부클래스의 이름 = {}", template2.getClass());
+
+        template1.execute();
+        template2.execute();
     }
 }
--- src/test/java/hello/advanced/trace/template/code/AbstractTemplate.java
+++ src/test/java/hello/advanced/trace/template/code/AbstractTemplate.java
@@ -2,6 +2,18 @@ package hello.advanced.trace.template.code;
 
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * 템플릿 메서드 패턴
+ * 상위 클래스에 알고리즘의 기본 골격(템플릿)을 정의하고, 하위 클래스에서 알고리즘의 세부 단계를 구현.
+ * 즉, 공통된 로직은 상위 클래스에서 처리하고, 변화하는 부분은 하위 클래스에서 오버라이딩하여 구현
+ * execute() : 공통 부분
+ * call() : 구체적인 로직.
+ *      protected로 외부에서의 호출을 막는 이유?
+ *      execute까지 세트로 동작해야 하는 메서드인데 call 부분만 떼어내서 사용하는 일 방지.
+ *      정해진 흐름대로만 작동하도록 강제.
+ * SubClassLogic1, 2를 만들 필요 없이 익명 클래스로 구현하면 편하다.
+ * 단, 람다식은 불가능. 람다식은 메서드가 하나만 있는 함수형 인터페이스에만 적용 가능하다.
+ */
 @Slf4j
 public abstract class AbstractTemplate {
 
@@ -15,6 +27,11 @@ public abstract class AbstractTemplate {
         long resultTime = endTime - startTime;
         log.info("resultTime = {}", resultTime);
     }
+
+    /*
+     자식 클래스가 구체적인 로직을 작성하는 부분.
+     */
+
 
     protected abstract void call();
 }
