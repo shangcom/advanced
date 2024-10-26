22599b56d321b078a996c0818a592e9ef37cc27e
shangcom <dndbs91@gmail.com>
2024-10-26 15:05:04 +0900

    스프링 핵심 원리 - 고급편
    섹션4 - 템플릿 메서드 패턴과 콜백 패턴
    
    템플릿 콜백 패턴 - 예제
    
    완료

---
+++ src/test/java/hello/advanced/trace/strategy/code/TemplateCallbackTest.java
@@ -0,0 +1,43 @@
+package hello.advanced.trace.strategy.code;
+
+import hello.advanced.trace.strategy.code.template.Callback;
+import hello.advanced.trace.strategy.code.template.TimeLogTemplate;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class TemplateCallbackTest {
+
+    /**
+     * 템플릿 콜백 패턴 - 익명 내부 클래스
+     */
+    @Test
+    void callbackV11() {
+
+        TimeLogTemplate timeLogTemplate = new TimeLogTemplate();
+        timeLogTemplate.execute(new Callback() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        });
+
+        timeLogTemplate.execute(new Callback() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        });
+    }
+
+    /**
+     * 템플릿 콜백 패턴 - 익명 내부 클래스
+     */
+    @Test
+    void callbackV2() {
+
+        TimeLogTemplate timeLogTemplate = new TimeLogTemplate();
+        timeLogTemplate.execute(() -> log.info("비즈니스 로직1 실행"));
+        timeLogTemplate.execute(() -> log.info("비즈니스 로직2 실행"));
+    }
+}
--- src/test/java/hello/advanced/trace/strategy/code/strategy/ContextV2.java
+++ src/test/java/hello/advanced/trace/strategy/code/strategy/ContextV2.java
@@ -3,10 +3,14 @@ package hello.advanced.trace.strategy.code.strategy;
 import lombok.extern.slf4j.Slf4j;
 
 /**
+ * 템플릿 콜백 패턴
  * 전략을 파라미터로 전달받아 사용.
- * 전략을 필드에 주입 받아 사용할 때와 달리, 컨텍스트 객체는 하나만 만들고
+ * 전략을 필드에 주입 받아 사용할 때와 달리, 동일한 Context 인스턴스를 사용하면서
  * execute(Strategy strategy)에 전략은 인자로 전달 받음.
- * 즉 실행 시점에 전략을 결정할 수 있음.
+ * 즉 실행 시점에 동적으로 전략을 결정할 수 있음.
+ * callback?
+ * 콜백은 특정 시점에 실행할 코드를 미리 넘겨두고(call), 나중에 필요할 때(back) 그 코드를 호출하는 방식.
+ * 특정 인터페이스의 구현체를 (익명 클래스나 람다식)인스턴스화해서 콜백으로 넘긴다
  */
 @Slf4j
 public class ContextV2 {
